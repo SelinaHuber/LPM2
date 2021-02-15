8f905bab4c2be816971b4d49a283078ddbf02e8f
Seamus O'Sullivan <info@seamuso.com>
2021-02-15 18:03:11 +0100

    path

--- LanguageProject/routes/dashboard.js
+++ LanguageProject/routes/dashboard.js
@@ -8,11 +8,12 @@ axios.defaults.headers.common["app_key"] = process.env.keyApi;
 
 router.get("/dashboard", (req, res, next) => {
   // console.log('REQQQQQQQQQQQQQQ');
-  axios.get('https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/dog?strictMatch=false')
+  axios.get('https://od-api.oxforddictionaries.com/api/v2/translations/en/de/dog?strictMatch=false')
     .then(def => {
-      console.log('DEFFFFF', def.data.results[0].id);
-      const definition = def.data.results[0].id;
-      res.render("dashboard", { definition });
+      console.log('DEFFFFF', def.data.results[0].lexicalEntries[0].entries[0].senses[0].translations[0].text);
+      const searchedWord = def.data.results[0].id;
+      const translatedWord = def.data.results[0].lexicalEntries[0].entries[0].senses[0].translations[0].text
+      res.render("dashboard", { searchedWord, translatedWord });
     })
     .catch(err => {
       console.log(err);
